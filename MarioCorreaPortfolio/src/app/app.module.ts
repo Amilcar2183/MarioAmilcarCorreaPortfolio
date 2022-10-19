7405501e5c042c022d70f85b46f431e86dd67492
MarioAmilcarCorrea <amilcar2183@gmail.com>
2022-10-19 18:51:40 -0300

    Inicio de Estructura

---
+++ MarioCorreaPortfolio/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    EncabezadoComponent,
+    ExperienciaComponent,
+    HabilidadesComponent,
+    ProyectosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
